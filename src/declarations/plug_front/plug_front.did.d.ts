e990678fc315060615ceb5f9dba6aea48fd2a383
pontagon333 <hironosu.work@gmail.com>
2022-01-04 16:59:47 +0900

    Add candid files

--- src/declarations/plug_front/plug_front.did.d.ts
+++ src/declarations/plug_front/plug_front.did.d.ts
@@ -1,2 +1,10 @@
 import type { Principal } from '@dfinity/principal';
-export interface _SERVICE { 'greet' : (arg_0: string) => Promise<string> }
+export interface Attribute { 'key' : string, 'value' : string }
+export interface Small_NFT {
+  'mint' : (arg_0: Principal, arg_1: [] | [TokenMetadata]) => Promise<TokenID>,
+  'ownerOf' : (arg_0: TokenID) => Promise<string>,
+  'transfer' : (arg_0: Principal, arg_1: TokenID) => Promise<TokenID>,
+}
+export type TokenID = bigint;
+export interface TokenMetadata { 'attributes' : [] | [Array<Attribute>] }
+export interface _SERVICE extends Small_NFT {}
--- src/declarations/plug_front/plug_front.did.js
+++ src/declarations/plug_front/plug_front.did.js
@@ -1,4 +1,14 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], []) });
+  const Attribute = IDL.Record({ 'key' : IDL.Text, 'value' : IDL.Text });
+  const TokenMetadata = IDL.Record({
+    'attributes' : IDL.Opt(IDL.Vec(Attribute)),
+  });
+  const TokenID = IDL.Nat;
+  const Small_NFT = IDL.Service({
+    'mint' : IDL.Func([IDL.Principal, IDL.Opt(TokenMetadata)], [TokenID], []),
+    'ownerOf' : IDL.Func([TokenID], [IDL.Text], []),
+    'transfer' : IDL.Func([IDL.Principal, TokenID], [TokenID], []),
+  });
+  return Small_NFT;
 };
 export const init = ({ IDL }) => { return []; };
